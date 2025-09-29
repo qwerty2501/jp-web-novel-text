3b2c0c3b5c689034a11dee995ef22f715f0dff4d
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-29 22:26:13 +0900

    feat: implemented dictionary parse

--- src/parser/general_parser.rs
+++ src/parser/general_parser.rs
@@ -1,7 +1,7 @@
 use nom::Input;
 
 use crate::{
-    DictionaryPhrase, Phrase,
+    DictionaryPhrase, Phrase, PlainPhrase,
     dictionary::Word,
     parser::{
         Result,
@@ -20,7 +20,7 @@ impl<S, X, WD> Parser<S, X, WD>
 where
     WD: WordContainer<S, X>,
 {
-    pub fn new(words: Vec<WD>) -> Result<Self> {
+    pub fn new_with_dic(words: Vec<WD>) -> Result<Self> {
         Ok(Parser {
             dictionary: DoubleArrayDictionary::<S, X, WD>::new(words)?,
         })
@@ -35,6 +35,8 @@ where
         ParseIter {
             text,
             dictionary: &self.dictionary,
+            plain_cache: None,
+            next_phrase: None,
         }
     }
 }
@@ -46,6 +48,8 @@ where
 {
     text: &'a S,
     dictionary: &'a DoubleArrayDictionary<S, X, WD>,
+    plain_cache: Option<&'a S>,
+    next_phrase: Option<Phrase<&'a S, &'a Word<X>>>,
 }
 
 impl<'a, S, X, WD> ParseIter<'a, S, X, WD>
@@ -53,16 +57,30 @@ where
     &'a S: Input<Item = char>,
     WD: WordContainer<S, X>,
 {
-    fn parse(&mut self) -> Option<Phrase<&'a S, &'a Word<X>>> {
+    #[inline]
+    fn parse_high_priority_once(&mut self) -> Option<(Phrase<&'a S, &'a Word<X>>, &'a S)> {
+        unimplemented!()
+    }
+
+    #[inline]
+    fn parse_once(&mut self) -> Option<(Phrase<&'a S, &'a Word<X>>, &'a S)> {
+        if let Some(r) = self.parse_high_priority_once() {
+            Some(r)
+        } else {
+            self.parse_dictionary_phrase_once()
+        }
+    }
+
+    #[inline]
+    fn parse_dictionary_phrase_once(&mut self) -> Option<(Phrase<&'a S, &'a Word<X>>, &'a S)> {
         if let Some(wc) = self.dictionary.get(self.text.iter_elements()) {
             let (fragment, text) = self.text.take_split(wc.input_key_len());
-            self.text = text;
-            Some(Phrase::new_dictionary_word(DictionaryPhrase::new(
-                fragment,
-                wc.word(),
-            )))
+            Some((
+                Phrase::new_dictionary_word(DictionaryPhrase::new(fragment, wc.word())),
+                text,
+            ))
         } else {
-            unimplemented!()
+            None
         }
     }
 }
@@ -74,6 +92,24 @@ where
 {
     type Item = Phrase<&'a S, &'a Word<X>>;
     fn next(&mut self) -> Option<Self::Item> {
-        self.parse()
+        if let Some(next) = &self.next_phrase {
+            let next = next.clone();
+            self.next_phrase = None;
+            Some(next)
+        } else if let Some((phrase, next)) = self.parse_once() {
+            self.text = next;
+            if let Some(plain) = self.plain_cache {
+                self.next_phrase = Some(phrase);
+                Some(Phrase::new_plain(PlainPhrase::new(plain)))
+            } else {
+                Some(phrase)
+            }
+        } else {
+            if self.plain_cache.is_none() {
+                self.plain_cache = Some(self.text);
+            }
+            self.text = self.text.take_from(1);
+            None
+        }
     }
 }
--- src/phrase.rs
+++ src/phrase.rs
@@ -1,7 +1,7 @@
 use derive_getters::Getters;
 use derive_new::new;
 
-#[derive(new)]
+#[derive(new, Clone)]
 pub enum Phrase<S, DW> {
     Ruby(RubyPhrase<S>),
     DictionaryWord(DictionaryPhrase<S, DW>),
@@ -9,25 +9,25 @@ pub enum Phrase<S, DW> {
     Plain(PlainPhrase<S>),
 }
 
-#[derive(Getters, new)]
+#[derive(Getters, new, Clone)]
 pub struct RubyPhrase<S> {
     fragment: S,
     target: S,
     ruby: S,
 }
 
-#[derive(Getters, new)]
+#[derive(Getters, new, Clone)]
 pub struct DictionaryPhrase<S, DW> {
     fragment: S,
     word: DW,
 }
 
-#[derive(Getters, new)]
+#[derive(Getters, new, Clone)]
 pub struct PlainPhrase<S> {
     fragment: S,
 }
 
-#[derive(Getters, new)]
+#[derive(Getters, new, Clone)]
 pub struct NewLinePhrase<S> {
     fragment: S,
 }
