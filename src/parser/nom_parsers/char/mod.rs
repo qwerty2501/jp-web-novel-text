e2011043a4a95da89e2389db5206afca641a7619
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-10-01 01:03:51 +0900

    implemented decision function for ruby

--- src/parser/nom_parsers/char/mod.rs
+++ src/parser/nom_parsers/char/mod.rs
@@ -9,6 +9,18 @@ pub(crate) const fn is_tab(c: char) -> bool {
     c == '\t'
 }
 
+pub(crate) const fn is_start_instruction(c: char) -> bool {
+    c == '|' || c == '｜'
+}
+
+pub(crate) const fn is_start_ruby(c: char) -> bool {
+    c == '(' || c == '（' || c == '《' || c == '⟪'
+}
+
+pub(crate) const fn is_end_ruby(c: char) -> bool {
+    c == ')' || c == '）' || c == '》' || c == '⟫'
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +58,58 @@ mod tests {
     #[case('あ', false)]
     fn is_tab_works(#[case] c: char, #[case] expected: bool) {
         assert_that!(is_tab(c), eq(expected))
+    }
+    #[gtest]
+    #[rstest]
+    #[case(' ', false)]
+    #[case('　', false)]
+    #[case('\t', false)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    #[case('|', true)]
+    #[case('｜', true)]
+    fn is_start_instruction_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_start_instruction(c), eq(expected))
+    }
+
+    #[gtest]
+    #[rstest]
+    #[case(' ', false)]
+    #[case('　', false)]
+    #[case('\t', false)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    #[case('|', false)]
+    #[case('｜', false)]
+    #[case('(', true)]
+    #[case('（', true)]
+    #[case('《', true)]
+    #[case('⟪', true)]
+    #[case(')', false)]
+    #[case('）', false)]
+    #[case('》', false)]
+    #[case('⟫', false)]
+    fn is_start_ruby_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_start_ruby(c), eq(expected))
+    }
+    #[gtest]
+    #[rstest]
+    #[case(' ', false)]
+    #[case('　', false)]
+    #[case('\t', false)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    #[case('|', false)]
+    #[case('｜', false)]
+    #[case('(', false)]
+    #[case('（', false)]
+    #[case('《', false)]
+    #[case('⟪', false)]
+    #[case(')', true)]
+    #[case('）', true)]
+    #[case('》', true)]
+    #[case('⟫', true)]
+    fn is_end_ruby_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_end_ruby(c), eq(expected))
     }
 }
