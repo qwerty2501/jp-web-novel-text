0a0a8115220b17c8ed6f8a7e59875b88952d9899
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-10-01 00:48:22 +0900

    add test cases

--- src/parser/nom_parsers/char/mod.rs
+++ src/parser/nom_parsers/char/mod.rs
@@ -8,3 +8,43 @@ pub const fn is_zenkaku_space(c: char) -> bool {
 pub const fn is_tab(c: char) -> bool {
     c == '\t'
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use googletest::prelude::*;
+    use rstest::*;
+
+    #[gtest]
+    #[rstest]
+    #[case(' ', true)]
+    #[case('　', false)]
+    #[case('\t', false)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    fn is_space_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_space(c), eq(expected))
+    }
+
+    #[gtest]
+    #[rstest]
+    #[case(' ', false)]
+    #[case('　', true)]
+    #[case('\t', false)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    fn is_zenkaku_space_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_zenkaku_space(c), eq(expected))
+    }
+
+    #[gtest]
+    #[rstest]
+    #[case(' ', false)]
+    #[case('　', false)]
+    #[case('\t', true)]
+    #[case('a', false)]
+    #[case('あ', false)]
+    fn is_tab_works(#[case] c: char, #[case] expected: bool) {
+        assert_that!(is_tab(c), eq(expected))
+    }
+}
