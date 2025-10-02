323543df32d35982ad2d5d3e92eadace867459bb
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-10-03 00:55:21 +0900

    implemented phrase to serde

--- src/phrase.rs
+++ src/phrase.rs
@@ -2,6 +2,7 @@ use derive_getters::Getters;
 use derive_new::new;
 
 #[derive(new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Phrase<S, DW> {
     Ruby(RubyPhrase<S>),
     DictionaryWord(DictionaryPhrase<S, DW>),
@@ -11,12 +12,14 @@ pub enum Phrase<S, DW> {
 }
 
 #[derive(Copy, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum RubyType {
     Instruction,
     KanjiWithRuby,
 }
 
 #[derive(Getters, new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct RubyPhrase<S> {
     target: S,
     ruby: S,
@@ -24,28 +27,33 @@ pub struct RubyPhrase<S> {
 }
 
 #[derive(Getters, new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DictionaryPhrase<S, DW> {
     target: S,
     word: DW,
 }
 
 #[derive(Getters, new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct PlainPhrase<S> {
     target: S,
 }
 
 #[derive(Copy, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum NewLineType {
     Lf,
     CrLf,
 }
 
 #[derive(Getters, new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct NewLinePhrase {
     new_line_type: NewLineType,
 }
 
 #[derive(Copy, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum WhiteSpaceType {
     Space,
     ZenkakuSpace,
@@ -53,6 +61,7 @@ pub enum WhiteSpaceType {
 }
 
 #[derive(Getters, new, Clone, PartialEq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct WhiteSpacePhrase {
     count: usize,
     white_space_type: WhiteSpaceType,
